c2f68dec7e59aaf2c36b58c7ba668827840ad983
coaa0801-creator <68447827+coaa0801-creator@users.noreply.github.com>
2020-08-12 13:44:41 -0600

    Complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Aaron Cottrell";
+    int age = 30;
+    String state = "Minnesota";
+    String solution = "Name: " + name + " Age: " + age + " State: " + state;
+    System.out.println(solution);
+  }
+}
