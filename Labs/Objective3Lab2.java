73db8fbcb5780bcd0164cd9b3d5dbaa9f7e8ad0c
coaa0801-creator <68447827+coaa0801-creator@users.noreply.github.com>
2020-08-12 14:18:33 -0600

    Complete Objective3Lab2

---
+++ Labs/Objective3Lab2.java
@@ -0,0 +1,9 @@
+public class Objective3Lab2 {
+  public static void main(String[] args) {
+    double side1 = 4;
+    double side2 = 8;
+    double hypotenuse = side1 * side1 + side2 * side2;
+    double solution = Math.sqrt(hypotenuse);
+    System.out.println(solution);
+  }
+}
