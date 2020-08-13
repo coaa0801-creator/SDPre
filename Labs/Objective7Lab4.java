094ab9b17b0329072355a63b8df6d3dadb75af3f
coaa0801-creator <68447827+coaa0801-creator@users.noreply.github.com>
2020-08-13 13:08:35 -0600

    Complete Objective7Lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,11 @@
+public class Objective7Lab4 {
+  public static void main(String[] args) {
+    int num1 = 0;
+    int sum = 0;
+    while(num1 <= 20) {
+      sum = sum + num1;
+      num1 = num1 + 1;    
+    }
+    System.out.println(sum);
+  }
+}
