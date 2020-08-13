b6305913050ebf09f193e94030ef406b6855386d
coaa0801-creator <68447827+coaa0801-creator@users.noreply.github.com>
2020-08-13 13:37:34 -0600

    Complete Objective8Lab1

---
+++ Labs/Objective8Lab1.java
@@ -0,0 +1,10 @@
+public class Objective8Lab1 {
+  public static void main(String[] args) {
+    for(int num1 = 1; num1 <= 10; num1++) {
+      System.out.println(num1);
+    }
+    for(int num1 = 10; num1 >= 1; num1--) {
+      System.out.println(num1);
+    }
+  }
+}
