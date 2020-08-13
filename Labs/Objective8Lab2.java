7ae5bfa3dd62f3094a40e1deb8b3264877ed6e73
coaa0801-creator <68447827+coaa0801-creator@users.noreply.github.com>
2020-08-13 13:56:56 -0600

    Complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,8 @@
+public class Objective8Lab2 {
+  public static void main(String[] args){
+    int sum = 0;
+    for(int num1 = 1; num1 <= 20; num1++) {
+      sum = sum + num1;}
+    System.out.println(sum);
+  }
+}
